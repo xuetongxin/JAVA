637c6995e473e35f31983873e32647af3b12dc8e
xuetongxin <xsl203457@163.com>
2021-10-05 01:10:55 +0800

    delete 4

--- javafx/module-info.java
+++ javafx/module-info.java
@@ -1,10 +1,10 @@
 module javafx {
 	requires javafx.controls;
 	requires javafx.graphics;
-	requires javafx.base;
 	requires java.sql;
 	
 	opens application to javafx.graphics, javafx.fxml;
+	exports application;
 	exports Shape;
 	exports Cource_Design;
 }
