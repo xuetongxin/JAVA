4dd9a1cbeba48a6443407777e42372e86123251a
xuetongxin <xsl203457@163.com>
2021-10-05 01:08:15 +0800

    delete 2

--- javafx-archetype-fxml/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module org.openjfx.javafx_archetype_fxml {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens org.openjfx.javafx_archetype_fxml to javafx.fxml;
-    exports org.openjfx.javafx_archetype_fxml;
-}
