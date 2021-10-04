d1bff8458f6fb85b7a88ad7fed10e8060be2e50a
xuetongxin <xsl203457@163.com>
2021-10-05 01:14:11 +0800

    delete 7

--- WithModule/src/module-info.java
+++ WithModule/src/module-info.java
@@ -1,13 +1,8 @@
 module WithModule {
 	requires javafx.graphics;
-	//requires javafx.base;
 	requires javafx.controls;
-	//requires javafx.media;
-	//requires javafx.swing;
-	//requires javafx.web;
 	requires javafx.fxml;
 	
 	opens Wdemo to javafx.fxml;
 	exports Wdemo;
-	//exports ;
 }
